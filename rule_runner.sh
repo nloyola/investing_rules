53f62f7d421e203f2352c1bbe8d8e5028c7dafa6
Nelson Loyola <nloyola@gmail.com>
2025-05-20 23:34:58 +0200

    updated cron script

--- rule_runner.sh
+++ rule_runner.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /opt/nelson/src/investing/ticker_screener
-uv run rules.py rule-runner --json output.json > /dev/null 2>&1
+
+cd /opt/www-data/investing/ticker_screener
+#uv run rules.py rule-runner --json output.json > /dev/null 2>&1
+/home/nelson/.local/bin/uv run rules.py rule-runner --json output.json
